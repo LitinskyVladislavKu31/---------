794382b0cd74a578c41c90e494879476e0b1b0f9
LitinskyVladislavKu31 <145485808+LitinskyVladislavKu31@users.noreply.github.com>
2024-07-11 12:55:11 +0300

    Update main.py

--- bot 1/main.py
+++ bot 1/main.py
@@ -162,5 +162,5 @@ def handle_text_messages(message):
 
 if __name__ == '__main__':
     bot.remove_webhook()
-    bot.set_webhook(url='https://client-bot-three.vercel.app/' + bot.token)
+    bot.set_webhook(url='https://your-vercel-domain.vercel.app/' + bot.token)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
